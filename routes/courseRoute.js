e330fccd8c290fb29b1387b1fa8d67d52a774e6f
XeyberIgideli <d3facerx@gmail.com>
2023-05-08 00:04:13 +0400

    file uploading

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -10,7 +10,7 @@ router.get('/:slug',CourseOperations.getCourse)
 router.post('/enroll',CourseOperations.enrollCourse)
 router.post('/release',CourseOperations.releaseCourse)
 router.post('/:slug',CourseOperations.deleteCourse)
-router.post('/course/:slug',CourseOperations.sendMessage)
+router.post('/comment/:slug',CourseOperations.sendMessage)
 // router.post('/courses/new',CourseOperations.new) 
 // Bele olsaydi link bu cur olardi: http://localhost:3000/courses/new
 export default router
